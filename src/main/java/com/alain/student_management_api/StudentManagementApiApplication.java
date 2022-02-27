2abb0e37bce08a994e757933985afce7406aaeab
Alain Picard <devcoder0x0@gmail.com>
2022-02-26 18:40:45 -0800

    Add CommandLineRunner to main class [incomplete]

--- src/main/java/com/alain/student_management_api/StudentManagementApiApplication.java
+++ src/main/java/com/alain/student_management_api/StudentManagementApiApplication.java
@@ -1,13 +1,22 @@
 package com.alain.student_management_api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudentManagementApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApiApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run() {
+		return args -> {
+
+		};
 	}
 
 }
