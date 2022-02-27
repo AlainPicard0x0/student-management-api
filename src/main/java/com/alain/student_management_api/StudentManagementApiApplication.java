8021c9164ca0ea30e472060c729593bca052cfd5
Alain Picard <devcoder0x0@gmail.com>
2022-02-26 20:23:43 -0800

    Add CommandLineRunner to StudentManagementApiApplication class to load students into database when application starts up

--- src/main/java/com/alain/student_management_api/StudentManagementApiApplication.java
+++ src/main/java/com/alain/student_management_api/StudentManagementApiApplication.java
@@ -1,21 +1,34 @@
 package com.alain.student_management_api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 @SpringBootApplication
 public class StudentManagementApiApplication {
+
+	// Inject our StudentRepository interface so that we can use the saveAll() method in our CommandLineRunner run method below
+	@Autowired
+	private StudentRepository studentRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApiApplication.class, args);
 	}
 
+	// Add Students to database when application starts up
 	@Bean
 	public CommandLineRunner run() {
 		return args -> {
-
+			Student alain = new Student("Alain", "Picard", "alain@gmail", LocalDate.of(1999, Month.FEBRUARY, 26));
+			Student elon = new Student("elon", "musk", "elon@gmail", LocalDate.of(1985, Month.MARCH, 15));
+			Student michelle = new Student("Michelle", "Obama", "michelle@gmail.com", LocalDate.of(1984, Month.JULY, 5));
+			studentRepository.saveAll(List.of(alain, elon, michelle));
 		};
 	}
 
