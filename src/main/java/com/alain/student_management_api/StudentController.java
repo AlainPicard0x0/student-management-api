ace6406bc2736a722aa4d4234ed825299abaebea
Alain Picard <devcoder0x0@gmail.com>
2022-02-27 20:12:55 -0800

    Add comments to StudentController class

--- src/main/java/com/alain/student_management_api/StudentController.java
+++ src/main/java/com/alain/student_management_api/StudentController.java
@@ -13,21 +13,25 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
+    // Handle GET requests at route: api/student (Return list of all students).
     @GetMapping
     public List<Student> getStudents() {
         return studentService.findStudents();
     }
 
+    // Handle GET requests at route: api/student/{studentId} (Find individual student by their id).
     @GetMapping(path = "{studentId}")
     public Student getStudent(@PathVariable("studentId") Long id) {
         return studentService.findStudent(id);
     }
 
+    // Handle POST requests at route: api/student (Create new student and Save to database).
     @PostMapping
     public void addNewStudent(@RequestBody Student student) {
         studentService.addStudent(student);
     }
 
+    // Handle DELETE requests at route: api/student/{studentId} (Find student by their id and Delete student from database).
     @DeleteMapping(path = "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long id) {
         studentService.deleteStudent(id);
